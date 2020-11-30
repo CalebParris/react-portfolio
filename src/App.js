c79a32365a23f961cf2a222ea09591eb625c974c
Caleb Parris <calebparris@live.com>
2020-11-30 13:26:11 -0600

    Added Header, Footer, and basic files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Portfolio from "./pages/Portfilio";
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
+// import Portfolio from "./pages/Portfolio";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper"
@@ -14,10 +14,10 @@ function App() {
       <div>
         <Navbar />
         <Wrapper>
-          <Route exact path="/" component={About} />
+          {/* <Route exact path="/" component={About} />
           <Route exact path="/about" component={About} />
           <Route exact path="/contact" component={Contact} />
-          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/portfolio" component={Portfolio} /> */}
         </Wrapper>
         <Footer />
       </div>
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Link className="navbar-brand" to="/">
+        Caleb Parris
+      </Link>
+      <Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  </Link>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/about"
+              className={window.location.pathname === "/about" ? "nav-link active" : "nav-link"}
+            >
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/portfolio"
+              className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/contact"
+              className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
