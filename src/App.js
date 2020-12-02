f72ea0c879e19bdd1201eca47571dde93853735b
Caleb Parris <calebparris@live.com>
2020-12-01 22:05:07 -0600

    Added projects to portfolio page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-// import Portfolio from "./pages/Portfolio";
+import Portfolio from "./pages/Portfolio";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper"
@@ -17,7 +17,7 @@ function App() {
           <Route exact path="/" component={About} />
           <Route exact path="/about" component={About} />
           <Route exact path="/contact" component={Contact} />
-          {/* <Route exact path="/portfolio" component={Portfolio} /> */}
+          <Route exact path="/portfolio" component={Portfolio} />
         </Wrapper>
         <Footer />
       </div>
--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,22 +1,16 @@
-// import React from "react";
-// import CardContext from "../../utils/CardContext"
-// import "./style.css"
+import React from "react";
+import "./style.css"
 
-// function ProjectCard(){
-//     return(
-//         <CardContext.Consumer>
-//             {({ title, url, pageLink, depLink }) => {
-//                 return(
-//                     <div className="card">
-//                     <h3>{title}</h3>
-//                     <img src={url} alt="Screenshot of Project"/>
-//                 <a href={pageLink} target="_blank">Github Page</a>
-//                 <a href={depLink} target="_blank">Deployed Link</a>
-//                    </div>
-//                 )
-//             }}
-//         </CardContext.Consumer>
-//     )
-// }
+function ProjectCard({ title, url, pageLink, depLink }){
+    
+    return(
+        <div className="card">
+                    <h3>{title}</h3>
+                    <img src={url} alt="Screenshot of Project"/>
+                <a href={pageLink}>Github Page</a>
+                <a href={depLink}>Deployed Site</a>
+                   </div>
+    )
+}
 
-// export default ProjectCard;
+export default ProjectCard;
